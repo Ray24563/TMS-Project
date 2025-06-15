46048c77e99463ff3c230cac4fecf7ae0d20b77f
aernika <mn.olmedo4@gmail.com>
2025-06-15 14:54:39 +0800

    Configurations for deployment - Added Dockerfile - Changed base url - Configured Database.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/task-management/public/';
+    public string $baseURL = 'https://task-management-kfun.onrender.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
